487d9d2c6ae0893583ad6ad6d4c757a8bc766ca0
Francesco Aiello <aiellof@oregonstate.edu>
2021-02-04 14:15:20 -0800

    Add bucket selection UI elements

--- views/music_screen/music_screen.py
+++ views/music_screen/music_screen.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QGridLayout, QLabel, QWidget, QLineEdit
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QComboBox, QGridLayout, QLabel, QPushButton, QWidget, QLineEdit
 
 
 class MusicScreen(QWidget):
@@ -9,9 +10,22 @@ class MusicScreen(QWidget):
 
     def init_ui(self):
         self.create_aws_profile_specification()
+        self.create_bucket_dropdown()
+
+    def create_bucket_dropdown(self):
+        bucket_label = QLabel("S3 Bucket")
+        self.bucket_dropdown = QComboBox()
+        self.refresh_bucket = QPushButton() 
+        self.refresh_bucket.setIcon(QIcon('views/assets/refresh.png'))
+        self.refresh_bucket.clicked.connect(self.populate_bucket_dropdown)
+        
+        self.grid_layout.addWidget(bucket_label, 1, 0)
+        self.grid_layout.addWidget(self.bucket_dropdown, 1, 1)
+        self.grid_layout.addWidget(self.refresh_bucket, 1, 2) 
+  
 
     def populate_bucket_dropdown(self):
-        pass
+        print("hi")
     
     def create_aws_profile_specification(self):
         aws_profile_label = QLabel("AWS Profile (leave blank for default)")
