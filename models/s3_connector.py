b5f04310fe4d17372ce5951cb7b8c82f9a096ca2
Francesco Aiello <aiellof@oregonstate.edu>
2021-02-04 11:53:47 -0800

    Add directory uploading with tests to verify behavior

--- models/s3_connector.py
+++ models/s3_connector.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 
 
 class S3Connector:
@@ -17,7 +18,7 @@ class S3Connector:
             return bucket.name
             
     def list_bucket_content(self, bucket_name, directory=''):
-        response = self.s3_client.list_objects(Bucket="foobucket")
+        response = self.s3_client.list_objects(Bucket="foobucket", Prefix="")
         contents = []
         for content in response.get('Contents', []):
             contents.append(content.get('Key'))
@@ -28,3 +29,15 @@ class S3Connector:
         with open(kwargs["file_path"], 'rb') as data:
             self.s3_client.upload_fileobj(data, kwargs["bucket_name"], kwargs["file_name"])
             
+    
+    def upload_directory(self, **kwargs):
+        path = kwargs["directory_path"]
+        bucket = self.s3_resource.Bucket(kwargs["bucket_name"])
+        aws_directory = kwargs["aws_directory"]
+        
+        for sub_directory, directory, files in os.walk(path):
+            for file in files:
+                full_path = os.path.join(sub_directory, file)
+                with open(full_path, 'rb') as data:
+                    full_aws_path = aws_directory + '/' + full_path[len(path)+1:]
+                    bucket.put_object(Key=full_aws_path, Body=data)
--- test/test_s3_connector.py
+++ test/test_s3_connector.py
@@ -97,3 +97,24 @@ class TestS3Connector(unittest.TestCase):
             contents.append(content.get('Key'))
             
         self.assertEqual(contents, ["foofile"])
+
+    @mock_s3
+    def test_upload_directory_to_s3_bucket(self):
+        """
+        An S3 Connector should successfully upload a directory of files to a specified bucket
+        """
+        conn = boto3.resource('s3', region_name='us-east-1')
+        # We need to create the bucket since this is all in Moto's 'virtual' AWS account
+        conn.create_bucket(Bucket='foobucket')
+        
+        s3_connector = S3Connector()
+        s3_connector.connect("default")
+        s3_connector.upload_directory(directory_path="test/test_resources/test_directory", bucket_name="foobucket", aws_directory="test_directory")
+        
+        # get bucket contents
+        response = boto3.client('s3').list_objects(Bucket="foobucket")
+        contents = []
+        for content in response.get('Contents', []):
+            contents.append(content.get('Key'))
+        
+        self.assertEqual(contents, ["test_directory/test_file", "test_directory/test_file2"])
