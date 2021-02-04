1353af48a0a90b538e09b762579c9c547f11e92a
Francesco Aiello <aiellof@oregonstate.edu>
2021-02-04 10:17:34 -0800

    Add instantiation member variable tests

--- test/test_s3_connector.py
+++ test/test_s3_connector.py
@@ -8,9 +8,20 @@ class TestBinarySearchTree(unittest.TestCase):
 
     def test_instantiation(self):
         """
-        A S3 Connector exists
+        An S3 Connector exists
         """
         try:
             S3Connector()
         except NameError:
             self.fail("Could not instantiate S3Connector")
+            
+            
+    def test_instantiation_member_variables(self):
+        """
+        An S3 Connector should not have a session or s3 upon initialization
+        """
+        session = None
+        s3 = None
+        s3_connector = S3Connector()
+        self.assertEqual(s3_connector.session, session)
+        self.assertEqual(s3_connector.s3, s3)
