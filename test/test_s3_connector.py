fe84517067a1640848dc240c70590d2457e610fe
Francesco Aiello <aiellof@oregonstate.edu>
2021-02-04 10:27:18 -0800

    Add test for connecting to mock aws s3

--- test/test_s3_connector.py
+++ test/test_s3_connector.py
@@ -1,4 +1,6 @@
 import unittest
+import boto3
+from moto import mock_s3
 from models.s3_connector import S3Connector
 
 class TestS3Connector(unittest.TestCase):
@@ -25,3 +27,22 @@ class TestS3Connector(unittest.TestCase):
         s3_connector = S3Connector()
         self.assertEqual(s3_connector.session, session)
         self.assertEqual(s3_connector.s3, s3)
+        
+    
+    @mock_s3
+    def test_connect_to_aws_s3(self):
+        """
+        An S3 Connector should successfully connect to aws s3
+        """
+        conn = boto3.resource('s3', region_name='us-east-1')
+        # We need to create the bucket since this is all in Moto's 'virtual' AWS account
+        conn.create_bucket(Bucket='foobucket')
+        
+        s3_connector = S3Connector()
+        
+        try:
+            s3_connector.connect("default")
+        except:
+            self.fail("Could not connect to aws using mock aws s3")
+            
+    
