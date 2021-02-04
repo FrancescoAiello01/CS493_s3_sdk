18999b2161da1970b67890fa807e15acc5d237eb
Francesco Aiello <aiellof@oregonstate.edu>
2021-02-04 14:07:19 -0800

    Add AWS profile selection widget

--- views/music_screen/music_screen.py
+++ views/music_screen/music_screen.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QGridLayout, QLabel, QWidget
+from PyQt5.QtWidgets import QGridLayout, QLabel, QWidget, QLineEdit
 
 
 class MusicScreen(QWidget):
@@ -8,5 +8,14 @@ class MusicScreen(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        test = QLabel("test")
-        self.grid_layout.addWidget(test, 2, 3)
+        self.create_aws_profile_specification()
+
+    def populate_bucket_dropdown(self):
+        pass
+    
+    def create_aws_profile_specification(self):
+        aws_profile_label = QLabel("AWS Profile (leave blank for default)")
+        self.aws_profile = QLineEdit()
+        self.grid_layout.addWidget(aws_profile_label, 0, 0)
+        self.grid_layout.addWidget(self.aws_profile, 0, 1)
+        
